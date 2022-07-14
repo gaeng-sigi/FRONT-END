c311bb513de058e5f2693fcd896729d8eb62d471
gaengsigi <elvin2@naver.com>
2022-07-14 11:11:54 +0900

    7/14 vue.js 1교시

---
+++ vue.js/vue-movie-pjt/src/mixins.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            baseUrl: 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/',
+            BoxOfficeByDay: 'boxoffice/searchDailyBoxOfficeList.json'
+        }
+    },
+
+    methods: {
+        async $api(url, data) {
+            return (await axios({
+                method: 'get',
+                url,
+                data
+            })).catch(e => {
+                console.log(e);
+            }).data;
+        },
+
+        async getBoxOfficeByDay() {
+            return await this.$api(this.baseUrl + this.BoxOfficeByDay, {});
+        }
+    }
+}
