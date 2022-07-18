b5df953eddf0d6f889c9884ecc42cb0646349ed7
gaengsigi <elvin2@naver.com>
2022-07-18 21:02:09 +0900

    7/18

--- vue.js/vue-shop-pjt/src/views/ProductCreate.vue
+++ vue.js/vue-shop-pjt/src/views/ProductCreate.vue
@@ -1,97 +1,97 @@
 <template>
     <main class="mt-3">
         <div class="container">
-        <h2 class="text-center">제품등록</h2>
-        
-        <div class="mb-3 row">
-            <label class="col-md-3 col-form-label">제품명</label>
-            <div class="col-md-9">
-                <input type="text" class="form-control" v-model="product.product_name">
-            </div>
-        </div>
-
-        <div class="mb-3 row">
-            <label class="col-md-3 col-form-label">제품가격</label>
-            <div class="col-md-9">
-                <div class="input-group mb-3">
-                    <input type="number" class="form-control" v-model="product.product_price">
-                    <span class="input-group-text">원</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="mb-3 row">
-            <label class="col-md-3 col-form-label">배송비</label>
-            <div class="col-md-9">
-                <div class="input-group mb-3">
-                    <input type="number" class="form-control" v-model="product.delivery_price">
-                    <span class="input-group-text">원</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="mb-3 row">
-            <label class="col-md-3 col-form-label">추가배송비(도서산간)</label>
-            <div class="col-md-9">
-                <div class="input-group mb-3">
-                    <input type="number" class="form-control" v-model="product.add_delivery_price">
-                    <span class="input-group-text">원</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="mb-3 row">
-            <label class="col-md-3 col-form-label">카테고리</label>
-            <div class="col-md-9">
+            <h2 class="text-center">제품등록</h2>
             
-                <div class="row">
-                    <div class="col-auto">              
-                        <select class="form-select" v-model="cate1" @change="changeCate1">
-                            <option :key="name" v-for="(value, name) of categoryObj">{{ name }}</option>
-                        </select>
-                    </div>
-                    
-                    <div class="col-auto" v-if="cate1 !== ''">
-                        <select class="form-select" v-model="cate2" @change="changeCate2">
-                            <option :key="name" v-for="(value, name) of categoryObj[cate1]">{{ name }}</option>
-                        </select>
-                    </div>
+            <div class="mb-3 row">
+                <label class="col-md-3 col-form-label">제품명</label>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" v-model="product.product_name">
+                </div>
+            </div>
 
-                    <div class="col-auto" v-if="cate2 !== ''">
-                        <select class="form-select" v-model="cate3">
-                            <option :key="idx" v-for="(cate, idx) in categoryObj[cate1][cate2]">{{ cate }}</option>
-                        </select>
+            <div class="mb-3 row">
+                <label class="col-md-3 col-form-label">제품가격</label>
+                <div class="col-md-9">
+                    <div class="input-group mb-3">
+                        <input type="number" class="form-control" v-model="product.product_price">
+                        <span class="input-group-text">원</span>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="mb-3 row">
-            <label class="col-md-3 col-form-label">태그</label>
-            <div class="col-md-9"></div>
-                <input type="text" class="form-control" v-model="product.tags">
-            </div>
-        </div>
-        
-        <div class="mb-3 row">
-            <label class="col-md-3 col-form-label">출고일</label>
-            <div class="col-md-9">
-                <div class="input-group mb-3">
-                    <input type="number" class="form-control" v-model="product.outbound_days">
-                    <span class="input-group-text">일 이내 출고</span>
+            <div class="mb-3 row">
+                <label class="col-md-3 col-form-label">배송비</label>
+                <div class="col-md-9">
+                    <div class="input-group mb-3">
+                        <input type="number" class="form-control" v-model="product.delivery_price">
+                        <span class="input-group-text">원</span>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="mb-3 row">
-            <div class="col-6 d-grid p-1">
-                <button type="button" class="btn btn-lg btn-dark" @click="goToList">취소</button>
+            <div class="mb-3 row">
+                <label class="col-md-3 col-form-label">추가배송비(도서산간)</label>
+                <div class="col-md-9">
+                    <div class="input-group mb-3">
+                        <input type="number" class="form-control" v-model="product.add_delivery_price">
+                        <span class="input-group-text">원</span>
+                    </div>
+                </div>
             </div>
-            <div class="col-6 d-grid p-1">
-                <button type="button" class="btn btn-lg btn-danger" @click="productInsert">저장</button>
-            </div>
-        </div>
 
+            <div class="mb-3 row">
+                <label class="col-md-3 col-form-label">카테고리</label>
+                <div class="col-md-9">
+                
+                    <div class="row">
+                        <div class="col-auto">              
+                            <select class="form-select" v-model="cate1" @change="changeCate1">
+                                <option :key="name" v-for="(value, name) of categoryObj">{{ name }}</option>
+                            </select>
+                        </div>
+                        
+                        <div class="col-auto" v-if="cate1 !== ''">
+                            <select class="form-select" v-model="cate2" @change="changeCate2">
+                                <option :key="name" v-for="(value, name) of categoryObj[cate1]">{{ name }}</option>
+                            </select>
+                        </div>
+
+                        <div class="col-auto" v-if="cate2 !== ''">
+                            <select class="form-select" v-model="cate3">
+                                <option :key="idx" v-for="(cate, idx) in categoryObj[cate1][cate2]">{{ cate }}</option>
+                            </select>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-3 row">
+                <label class="col-md-3 col-form-label">태그</label>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" v-model="product.tags">
+                </div>
+            </div>
+            
+            <div class="mb-3 row">
+                <label class="col-md-3 col-form-label">출고일</label>
+                <div class="col-md-9">
+                    <div class="input-group mb-3">
+                        <input type="number" class="form-control" v-model="product.outbound_days">
+                        <span class="input-group-text">일 이내 출고</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-3 row">
+                <div class="col-6 d-grid p-1">
+                    <button type="button" class="btn btn-lg btn-dark" @click="goToList">취소</button>
+                </div>
+                <div class="col-6 d-grid p-1">
+                    <button type="button" class="btn btn-lg btn-danger" @click="productInsert">저장</button>
+                </div>
+            </div>
         </div>
     </main>
 </template>
