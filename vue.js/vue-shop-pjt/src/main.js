c03688075ba6dedea2010fbdf4220d4b973e3bca
gaengsigi <elvin2@naver.com>
2022-07-19 11:43:04 +0900

    7/18 못한 부분

--- vue.js/vue-shop-pjt/src/main.js
+++ vue.js/vue-shop-pjt/src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import mixins from './mixins'
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 createApp(App)
 .mixin(mixins)
 .use(store)
 .use(router)
+.use(VueSweetalert2)
 .mount('#app')
 
 window.Kakao.init("c4bab976007a46fb60430c8c25bd5a16");
